15d09f529ba1a7b7b53ffb60e5cd8dc8ab3e7ac8
chrisdeckard2 <christopherdaviddeckard@gmail.com>
2024-06-17 11:44:06 -0400

    Add project files.

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GeometricShapes;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        sqr s1 = new sqr(6);
+        Console.WriteLine($"Perimeter is {s1.Perimeter()} and area is {s1.Area()}");
+
+
+        Rect r1 = new Rect(5, 10);
+        Console.WriteLine($"Perimeter is {r1.Perimeter()} and area is {r1.Area()}");
+
+
+        Quads q1 = new Quads(3, 4, 5, 6);
+
+        Console.WriteLine($"Perimeter is {q1.Perimeter()}");
+
+        Quads q2 = new Quads(11, 12, 13, 14);
+
+        Console.WriteLine($"Perimeter is {q2.Perimeter()}");
+
+    }
+}
---
+++ GeometricShapes/sqr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes;
+internal class sqr {
+
+    public int Side {  get; set; } = 0;
+
+    public int Perimeter() {
+        return 4 * Side;
+
+    }
+
+    public int Area() {
+        return Side * Side;
+    }
+
+    public sqr(int side) {
+        side = side;
+
+    }
+}
